f86af9ff161bea30558621b63e5f98a28834f7f4
Tristan Rechenberger <rechenberger@me.com>
2023-06-29 13:16:59 +0200

    🎉

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode'
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'teampilot-ai.refactor',
-    () => {
+    async () => {
       const editor = vscode.window.activeTextEditor
       if (editor) {
         const document = editor.document
@@ -13,11 +13,25 @@ export function activate(context: vscode.ExtensionContext) {
 
         const text = document.getText(selection)
 
+        // Get all diagnostics for the current file
+        const diagnostics = vscode.languages.getDiagnostics(document.uri)
+
+        // Filter diagnostics to only include those that are within the selected text
+        const errors = diagnostics
+          .filter(
+            (diagnostic) =>
+              diagnostic.range.start.isAfterOrEqual(selection.start) &&
+              diagnostic.range.end.isBeforeOrEqual(selection.end)
+          )
+          .map((diagnostic) => `${diagnostic.message}`)
+
         vscode.window
           .showInputBox({ prompt: 'Enter your refactor message' })
           .then((value) => {
             if (value !== undefined) {
-              const prompt = `${value} \n\`\`\`ts\n${text}\n\`\`\``
+              const prompt = `${value} \n\`\`\`ts\n${text}\n\`\`\`\n\nErrors:\n${errors.join(
+                '\n'
+              )}`
               const encodedPrompt = encodeURIComponent(prompt)
               const url = `https://teampilot.ai/new?prompt=${encodedPrompt}`
 
