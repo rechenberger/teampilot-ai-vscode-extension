02f0bc22590a7c2505b6b7d95c42388a39df4a45
Tristan Rechenberger <rechenberger@me.com>
2023-06-29 13:15:01 +0200

    🎉

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+/* eslint-disable @typescript-eslint/semi */
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import * as vscode from 'vscode'
+
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    'teampilot-ai.refactor',
+    () => {
+      const editor = vscode.window.activeTextEditor
+      if (editor) {
+        const document = editor.document
+        const selection = editor.selection
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "teampilot-ai" is now active!');
+        const text = document.getText(selection)
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('teampilot-ai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Teampilot AI!');
-	});
+        vscode.window
+          .showInputBox({ prompt: 'Enter your refactor message' })
+          .then((value) => {
+            if (value !== undefined) {
+              const prompt = `${value} \n\`\`\`ts\n${text}\n\`\`\``
+              const encodedPrompt = encodeURIComponent(prompt)
+              const url = `https://teampilot.ai/new?prompt=${encodedPrompt}`
 
-	context.subscriptions.push(disposable);
+              vscode.env.openExternal(vscode.Uri.parse(url))
+            }
+          })
+      }
+    }
+  )
+
+  context.subscriptions.push(disposable)
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
